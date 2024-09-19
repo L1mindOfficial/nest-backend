3b570ff8ad951151d521bb4f8bf39dc85c467c3a
Mohamadreza Mosalli <mohamadresaaa@gmail.com>
2024-09-19 18:32:10 +0330

    perf: move env module to common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
-import { EnvModule } from './env/env.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SessionsModule } from './sessions/sessions.module';
@@ -12,7 +11,6 @@ import { SessionsModule } from './sessions/sessions.module';
   imports: [
     CommonModule,
     DatabaseModule,
-    EnvModule,
     AuthModule,
     UsersModule,
     SessionsModule
