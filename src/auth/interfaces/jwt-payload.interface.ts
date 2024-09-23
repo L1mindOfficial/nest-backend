9c916128e5bfef226de37b0c62aa7544a2f015df
Mohamadreza Mosalli <mohamadresaaa@gmail.com>
2024-09-23 18:20:03 +0330

    feat: replace id to email in jwToken

--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -2,12 +2,12 @@
  * The `JwtPayload` interface defines the structure of the payload
  * contained within a JSON Web Token (JWT).
  *
- * This interface is used to ensure that the JWT payload has the
- * required properties for user identification.
+ * This interface ensures that the JWT payload includes essential
+ * user identification properties required for authentication.
  *
- * @property id - The unique identifier of the user, typically the user's database ID.
+ * @property email - The email of the user, used as a unique identifier for authentication purposes.
  */
 export interface JwtPayload {
-  // The unique identifier for the user
-  readonly id: string;
+  /** The email of the user, used as a unique identifier */
+  readonly email: string;
 }
