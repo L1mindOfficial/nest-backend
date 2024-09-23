60d16a471414ccfa81af70e0f19c587846b9cb05
Mohamadreza Mosalli <mohamadresaaa@gmail.com>
2024-09-23 11:38:43 +0330

    feat: overwrite user-agent decrator

--- src/auth/decorators/user-agent.decorator.ts
+++ src/auth/decorators/user-agent.decorator.ts
@@ -20,42 +20,56 @@ export const UserAgent = createParamDecorator(
     const request = ctx.switchToHttp().getRequest();
     const userAgent = request.headers['user-agent'];
 
-    let device: Device = {
+    // Default device initialization
+    const defaultDevice: Device = {
       name: 'unknown'
     };
 
-    if (userAgent) {
-      if (/like Mac OS X/.test(userAgent)) {
-        device = {
-          name: 'iOS',
-          version: /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/
-            .exec(userAgent)?.[2]
-            .replace(/_/g, '.')
-        };
-      } else if (/Android/.test(userAgent)) {
-        device = {
-          name: 'Android',
-          version: /Android ([0-9\.]+)[\);]/.exec(userAgent)?.[1]
-        };
-      } else if (/(Intel|PPC) Mac OS X/.test(userAgent)) {
-        device = {
-          name: 'macOS',
-          version: /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/
-            .exec(userAgent)?.[2]
-            .replace(/_/g, '.')
-        };
-      } else if (/Windows NT/.test(userAgent)) {
-        device = {
-          name: 'Windows',
-          version: /Windows NT ([0-9\._]+)[\);]/.exec(userAgent)?.[1]
-        };
-      } else if (/Linux/i.test(userAgent) && /X11/i.test(userAgent)) {
-        device = {
-          name: 'Linux'
+    if (!userAgent) {
+      return defaultDevice;
+    }
+
+    // Define device patterns
+    const devicePatterns = [
+      {
+        name: 'iOS',
+        regex: /like Mac OS X/,
+        versionRegex: /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/
+      },
+      {
+        name: 'Android',
+        regex: /Android/,
+        versionRegex: /Android ([0-9\.]+)[\);]/
+      },
+      {
+        name: 'macOS',
+        regex: /(Intel|PPC) Mac OS X/,
+        versionRegex: /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/
+      },
+      {
+        name: 'Windows',
+        regex: /Windows NT/,
+        versionRegex: /Windows NT ([0-9\._]+)[\);]/
+      },
+      {
+        name: 'Linux',
+        regex: /Linux/i,
+        versionRegex: null // No version regex for Linux
+      }
+    ];
+
+    // Check against patterns
+    for (const { name, regex, versionRegex } of devicePatterns) {
+      if (regex.test(userAgent)) {
+        return {
+          name,
+          version: versionRegex
+            ? userAgent.match(versionRegex)?.[1]?.replace(/_/g, '.')
+            : undefined
         };
       }
     }
 
-    return device;
+    return defaultDevice;
   }
 );
