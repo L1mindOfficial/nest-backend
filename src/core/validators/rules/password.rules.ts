e96f1d8ed2332b1afd7060828adfae602a47ea3a
mohamadreza mosalli <mohamadresaaa@gmail.com>
2026-01-07 13:56:51 +0330

    fix: password regex

--- src/core/validators/rules/password.rules.ts
+++ src/core/validators/rules/password.rules.ts
@@ -2,5 +2,5 @@ export const PASSWORD_MIN_LENGTH = 8;
 export const PASSWORD_MAX_LENGTH = 20;
 
 export const PASSWORD_REGEX = new RegExp(
-  `^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{${PASSWORD_MIN_LENGTH},${PASSWORD_MAX_LENGTH}}$`
+  `^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{${PASSWORD_MIN_LENGTH},${PASSWORD_MAX_LENGTH}}$`
 );
