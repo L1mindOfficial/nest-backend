9e73ac8f99dc3ca8ae99b5364858b46c135608ae
mohamadreza mosalli <mohamadresaaa@gmail.com>
2026-02-01 13:34:39 +0330

    fix: remove error files

--- src/features/users/errors/user-deletion-conflict-error.ts
+++
@@ -1,9 +0,0 @@
-import { DomainError } from 'core/exceptions/domain.error';
-
-export class UserDeletionConflictError extends DomainError {
-  readonly code = 'USER_DELETION_CONFLICT';
-
-  constructor(public readonly id: string) {
-    super(`User with ID ${id} cannot be deleted due to related data`);
-  }
-}
